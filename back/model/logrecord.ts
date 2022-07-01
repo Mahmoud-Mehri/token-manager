c133f84695abbe9e45d9677bedca6b56e9e75d48
Mahmoud-Mehri <Mahmoud.Mehri90@GMail.com>
2022-07-01 12:10:40 +0300

    LogRecord Model and Log Controller implemented

---
+++ back/model/logrecord.ts
@@ -0,0 +1,33 @@
+import mongoose from 'mongoose';
+import { LogRecordType } from './general';
+
+export interface ILogRecord {
+    logType: LogRecordType;
+    message: string;
+    timestamp: Date;
+}
+
+const LogRecordSchema = new mongoose.Schema<ILogRecord>({
+    logType: {
+        type: String,
+        enum: LogRecordType,
+        uppercase: true,
+        required: true
+    },
+    message: {
+        type: String,
+        required: true
+    },
+    timestamp: {
+        type: Date,
+        default: new Date(),
+        required: true
+    }
+})
+
+const LogRecordModel = mongoose.model('LogRecord', LogRecordSchema);
+
+export { LogRecordModel }
+
+
+
