e7e88fa0185137ec2a89da7bb57d1f75146d9426
Mahmoud-Mehri <Mahmoud.Mehri90@GMail.com>
2022-07-21 10:48:02 +0300

    Minor improvements

--- back/api/middleware/authenticator.ts
+++ back/api/middleware/authenticator.ts
@@ -1,7 +1,22 @@
 import { NextFunction, Request, Response } from 'express';
+import { newResponse } from '../../model/general';
+import { AuthController } from '../controller/auth-controller';
 
 export function authenticator(req: Request, res: Response, next: NextFunction) {
 
-    // ...
-    next();
+    const authController = new AuthController();
+
+    if (!req.session['userToken'])
+
+        if (req.headers && req.headers.authorization) {
+            const [name, token] = req.headers.authorization.split(' ');
+            if (name.toUpperCase() != 'TOKEN')
+                res.status(400).json(newResponse(false, "Authorization Failed!"))
+            else {
+                authController.verifyToken(token);
+                next()
+            }
+        } else {
+            res.status(400).json(newResponse(false, "Authorization Failed!"));
+        }
 }
