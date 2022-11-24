934c02acd7b65b9128dbaec1fd45b79ff3e345b9
Mahmoud Mehri <Mahmoud.Mehri90@gmail.com>
2022-11-25 01:50:58 +0300

    Temporary commented GraphQL Service

--- back/src/services/graphql-service.ts
+++ back/src/services/graphql-service.ts
@@ -1,22 +1,22 @@
-import express from "express";
-import { graphqlHTTP } from "express-graphql";
-import schema from "../graphql/schema";
-import { userResolvers, tokenResolvers } from "../graphql/resolvers";
+// import express from "express";
+// import { graphqlHTTP } from "express-graphql";
+// import schema from "../graphql/schema";
+// import { userResolvers, tokenResolvers } from "../graphql/resolvers";
 
-const rootResolvers = {
-  ...userResolvers,
-  ...tokenResolvers,
-};
+// const rootResolvers = {
+//   ...userResolvers,
+//   ...tokenResolvers,
+// };
 
-const graphqlApp = express();
+// const graphqlApp = express();
 
-graphqlApp.use(
-  "/graphql",
-  graphqlHTTP({
-    schema: schema,
-    rootValue: rootResolvers,
-    graphiql: true,
-  })
-);
+// graphqlApp.use(
+//   "/graphql",
+//   graphqlHTTP({
+//     schema: schema,
+//     rootValue: rootResolvers,
+//     graphiql: true,
+//   })
+// );
 
-export default graphqlApp;
+// export default graphqlApp;
