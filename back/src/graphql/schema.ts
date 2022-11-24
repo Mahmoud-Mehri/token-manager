551ec19144e3268a9640bd83c0df950f27177da3
Mahmoud Mehri <Mahmoud.Mehri90@gmail.com>
2022-11-25 01:35:36 +0300

    Removed GraphQL folder

--- back/src/graphql/schema.ts
+++
@@ -1,7 +0,0 @@
-import { buildSchema } from "graphql";
-import fs from "fs";
-
-const schemaString = fs.readFileSync("./schema.gql");
-const schema = buildSchema(`${schemaString}`);
-
-export default schema;
